ac32d177984d7706db17440fac3c185a5b1d168c
B K Jasila Fashmi <57026764+FASHMIJASILA@users.noreply.github.com>
2022-01-19 20:33:50 +0530

    Add files via upload

---
+++ Regular_Employee.java
@@ -0,0 +1,25 @@
+package com.cg.hibernatee.TablePerConcrete;
+
+
+	public class Regular_Employee extends Employee1{
+		private float salary;
+		private int bonus;
+
+		public float getSalary() {
+			return salary;
+		}
+		public void setSalary(float salary) {
+			this.salary = salary;
+		}
+		public int getBonus() {
+			return bonus;
+		}
+		public void setBonus(int bonus) {
+			this.bonus = bonus;
+		}
+
+
+		}
+
+
+
